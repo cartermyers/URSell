f5c3e50f3dfda3dc876047358a89fe779293e96e
Carter Myers <cartermyers123@gmail.com>
2017-11-07 01:31:28 +0000

    Switch debug to false

--- main/main/settings.py
+++ main/main/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8cp!a#zj+mv&^1^7g$d#93u5^t)ql9u5-%r#(8(8c47e=%pv_+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
 ALLOWED_HOSTS = ['cartermyers.pythonanywhere.com']
 
