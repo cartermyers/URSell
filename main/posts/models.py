c888085f09581a411d7a198c77231ecfc7d45653
Carter Myers <cartermyers123@gmail.com>
2017-11-14 10:40:14 -0600

    Create post image model

--- main/posts/models.py
+++ main/posts/models.py
@@ -6,6 +6,10 @@ from django.db import models
 class Posts(models.Model):
     title = models.CharField(max_length=255)
 
-class Categories(models.Model):
-    title = models.CharField(max_length=40) 
+# here is a simple model that allows us to store an arbitrary amount of images per post
+class PostImages(models.Model):
+    post = models.ForeignKey(Posts, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='posts/')
 
+class Categories(models.Model):
+    title = models.CharField(max_length=40)
