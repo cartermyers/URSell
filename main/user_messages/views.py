3e28acbd14a58a4f9f5c3dc5f0dfea540cdb4b5b
cartermyers <cartermyers123@gmail.com>
2017-11-20 18:56:36 -0600

    Add mailbox view prototype

--- main/user_messages/views.py
+++ main/user_messages/views.py
@@ -2,6 +2,21 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def mailbox(request):
+    # get type of mailbox:
+    box = request.GET.get('box', None)
+
+    if box == 'sent':
+        pass
+        #get sent mails
+    elif box == 'received':
+        pass
+        # get received mail
+    else:
+        pass
+        #put some kind of error here
+
     return render(request, 'user_messages/mailbox_test.html')
