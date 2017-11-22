d89c927fd51be0185e6544860c77aed72009a870
Carter Myers <cartermyers123@gmail.com>
2017-11-22 14:57:06 -0600

    Add email validation logic

--- main/account/urls.py
+++ main/account/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     url(r'^logout/$', views.logout_view, name='logout'),
     url(r'^validate_email/((?P<user_id>[0-9]+)/)?$', views.validate_email, name='validate_email'),
     url(r'^$', views.profile, name='myprofile'), #this is a logged in user's profile page
-    url(r'^(?P<category>[0-9]+)/$', views.profile, name='profile'),
+    url(r'^(?P<user_id>[0-9]+)/$', views.profile, name='profile'),
 ]
