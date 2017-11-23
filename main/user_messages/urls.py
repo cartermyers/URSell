425f430be8027a42987e4302d90a0703aaed8a28
Carter Myers <cartermyers123@gmail.com>
2017-11-22 19:29:42 -0600

    Add functioning message form

--- main/user_messages/urls.py
+++ main/user_messages/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.mailbox, name='mailbox'),
+    url(r'^send/$', views.send_user_mail, name='send_user_mail'),
 ]
--- main/user_messages/views.py
+++ main/user_messages/views.py
@@ -1,10 +1,33 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import messages
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
-from .models import SentMail, RecieveMail
+from .models import Mail, SentMail, RecieveMail, send_mail
+from account.models import User
+
+@login_required
+def send_user_mail(request):
+
+    if request.method =="POST":
+        to = request.POST['contact_name']
+        subject = request.POST['contact_subject']
+        message = request.POST['contact_message']
+
+        try:
+            to = User.objects.get(username=to)
+        except User.DoesNotExist:
+            messages.error(request, "Sorry! There's no user with that username.")
+            return HttpResponseRedirect(reverse('messages:mailbox'))
+
+        m = Mail(sender_id=request.user.pk, reciever_id=to.pk, subject=subject, content=message)
+        send_mail(m)
+
+    return HttpResponseRedirect(reverse('messages:mailbox'))
 
 @login_required
 def mailbox(request):
